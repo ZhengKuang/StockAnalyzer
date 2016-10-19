0ca8c0a91482e020e7aab84eab21d8d37f249950
ZhengKuang <kuangzheng2013212987@bupt.edu.cn>
2016-10-19 22:15:47 +0800

    add: the average price and write it back to kafka

--- spark/stream_process.py
+++ spark/stream_process.py
@@ -11,9 +11,22 @@ import json
 
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext    
-#from kafka import KafkaProducer
-#from kafka.errors import KafkaError
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
 from pyspark.streaming.kafka import KafkaUtils
+
+
+
+logger_format='%(asctime)-15s %(message)s'
+logging.basicConfig(format=logger_format)
+logger=logging.getLogger('data_producer')
+logger.setLevel(logging.DEBUG)
+
+
+kafka_broker='192.168.99.100:9092'
+kafka_topic='stock-analyzer'
+new_topic='test'
+
 
 
 def process(timeobj,rdd):
@@ -25,27 +38,44 @@ def process(timeobj,rdd):
 	# sum up all the price in the rdd
 	# - for each rdd, take out the LastTradeinPrice, json -> map 
 	# - for all the  rdd record, sum up -> reduce 
+
+	# - Exception handling
+
 	price_sum=rdd.map(lambda record: float(json.loads(record[1].decode('utf-8'))[0].get('LastTradePrice'))). reduce(lambda a,b:a + b )
-	print(price_sum)
-	print(number_of_records)
+	average=price_sum/number_of_records
+	logger.info('Received records from Kafka, average price is %f' %average)
+	current_time=time.time()
+	data=json.dumps({'timestamp':current_time,'average':average})
+	kafka_producer.send(new_topic,value=data) 
 
 if __name__ == '__main__':
-	if(len(sys.argv)!=3):
-		print("Not enough argument [kafka broker location], [kafka topic location]")
+	if(len(sys.argv)!=4):
+		print("Not enough argument [kafka broker location], [kafka topic location], [kafka new topic location]")
 		exit(1)
 
 	sc=SparkContext("local[2]","StockAveragePrice")
 	sc.setLogLevel('ERROR') 
 	ssc=StreamingContext(sc, 5 ) 
 
-	kafka_broker, kafka_topic=sys.argv[1:]
+	kafka_broker, kafka_topic, new_topic=sys.argv[1:]
 
 
 	# - setup a kafka stream
 	directKafkaStream=KafkaUtils.createDirectStream(ssc,[kafka_topic],{'metadata.broker.list':kafka_broker})
  	directKafkaStream.foreachRDD(process)
 
+ 	kafka_producer=KafkaProducer(bootstrap_servers=kafka_broker)
+
+
+ 	# - shutdown hook
 
 
 
+
+ 	ssc.start()
+ 	ssc.awaitTermination()
+
+
+
+ 
 
