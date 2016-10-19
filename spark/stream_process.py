2cc19534d05ec0cd71b886cff1fc7cce21abbe2f
ZhengKuang <kuangzheng2013212987@bupt.edu.cn>
2016-10-19 20:54:14 +0800

    add kafka producer, kafka analyzer, and spark analyzer

---
+++ spark/stream_process.py
@@ -0,0 +1,51 @@
+#coding=utf-8
+
+# - read from kafka
+# = do average 
+# - save data back
+
+import logging 
+import time
+import sys
+import json
+
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext    
+#from kafka import KafkaProducer
+#from kafka.errors import KafkaError
+from pyspark.streaming.kafka import KafkaUtils
+
+
+def process(timeobj,rdd):
+	# - do something
+	number_of_records=rdd.count()
+	#if number_of_records == 0:
+	if number_of_records==0:
+		return 
+	# sum up all the price in the rdd
+	# - for each rdd, take out the LastTradeinPrice, json -> map 
+	# - for all the  rdd record, sum up -> reduce 
+	price_sum=rdd.map(lambda record: float(json.loads(record[1].decode('utf-8'))[0].get('LastTradePrice'))). reduce(lambda a,b:a + b )
+	print(price_sum)
+	print(number_of_records)
+
+if __name__ == '__main__':
+	if(len(sys.argv)!=3):
+		print("Not enough argument [kafka broker location], [kafka topic location]")
+		exit(1)
+
+	sc=SparkContext("local[2]","StockAveragePrice")
+	sc.setLogLevel('ERROR') 
+	ssc=StreamingContext(sc, 5 ) 
+
+	kafka_broker, kafka_topic=sys.argv[1:]
+
+
+	# - setup a kafka stream
+	directKafkaStream=KafkaUtils.createDirectStream(ssc,[kafka_topic],{'metadata.broker.list':kafka_broker})
+ 	directKafkaStream.foreachRDD(process)
+
+
+
+
+
